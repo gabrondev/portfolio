4e211abfb554fd2a43cf10ed37cb130c913c5dba
Gabriel <gabrieltux19@gmail.com>
2025-03-23 09:46:42 -0300

    chore(frontend): atualizando portfólio para as minhas informações

--- frontend/src/components/curriculo/index.tsx
+++ frontend/src/components/curriculo/index.tsx
@@ -1,6 +1,6 @@
 import { Tecnologia } from "@core";
 import TecnologiasTrabalhadas from "./TecnologiasTrabalhadas";
-import Experiencia from "./Experiencia";
+// import Experiencia from "./Experiencia";
 import MiniCV from "./MiniCV";
 
 export interface CurriculoProps {
@@ -11,7 +11,7 @@ export default function Curriculo(props: CurriculoProps) {
     return (
         <div className="flex flex-col lg:flex-row gap-4 min-h-72 w-full">
             <MiniCV />
-            <Experiencia />
+            {/* <Experiencia /> */}
             <TecnologiasTrabalhadas lista={props.tecnologias} />
         </div>
     )
