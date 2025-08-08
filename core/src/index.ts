5714bef4efd2e0288738217221a8d215c5730696
Gabriel <gabrieltux19@gmail.com>
2025-08-08 19:11:18 -0300

    feat(core): adicionado tipo post no projeto

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./projeto"
 export * from "./tecnologia"
 export * from "./shared"
+export * from "./post"
---
+++ core/src/post/Post.ts
@@ -0,0 +1,8 @@
+export default interface Post {
+    id: number
+    titulo: string
+    slug: string
+    conteudo: string
+    criadoEm: Date
+    atualizadoEm: Date
+}
